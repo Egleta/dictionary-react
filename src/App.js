dabe94631f745343e75121f2755a0f37ae792008
Egleta <eglaite.t@gmail.com>
2021-11-12 16:44:50 +0100

    created Dictionary component and search field

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="container">
+                <main>
+                    <Dictionary />
+                </main>
+                <footer className="App-footer">
+                    <small>Coded by Egle Taraskeviciute</small>
+                </footer>
+            </div>
+        </div>
+    );
 }
 
 export default App;
