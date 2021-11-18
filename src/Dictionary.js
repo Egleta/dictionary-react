9f3b3f16476df0168a823a06be70fa57d90b5320
Egleta <eglaite.t@gmail.com>
2021-11-18 19:17:10 +0100

    added photos API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,30 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
-    function handleResponse(response) {
+    function handleDictionaryResponse(response) {
         setResults(response.data[0]);
+    }
+
+    function handlePexelsResponse(response) {
+        setPhotos(response.data.photos);
     }
 
     function search() {
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+        axios.get(apiUrl).then(handleDictionaryResponse);
+
+        let pexelsApiKey = "563492ad6f917000010000015c51bed2d63f4bbbb9f33802b8db0dd6";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+        let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+        axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     }
 
     function handleSubmit(event) {
@@ -26,7 +37,7 @@ export default function Dictionary(props) {
         setKeyword(event.target.value);
     }
 
-    function load(){
+    function load() {
         setLoaded(true);
         search();
     }
@@ -42,9 +53,10 @@ export default function Dictionary(props) {
                     <div className="hint">Suggested word: forest, book, cat, bicycle ...</div>
                 </section>
                 <Results results={results} />
+                <Photos photos={photos} />
             </div>
         );
-    } else{
+    } else {
         load();
         return "Loading";
     }
